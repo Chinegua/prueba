33c2df66d6d2ed9d0dc14721b6dd089635d93349
Chinegua <ai.chinegua@gmail.com>
2018-04-09 12:55:12 +0200

    Adding Jasmine, Karma y web view

---
+++ spec/jasmine_examples/PlayerSpec.js
@@ -0,0 +1,13 @@
+describe("Player", function() {
+
+
+  it("should be able to play a Song", function() {
+
+    expect(1).toEqual(1);
+  });
+
+  it("should be able to play a Song", function() {
+
+    expect(3).toEqual(1);
+  });
+});
